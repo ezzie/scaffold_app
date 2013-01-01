0c4ec0ba1096369210a52de20aa8f9b431bbd716
Rajan Punchouty <rajan@ezzie.in>
2013-01-01 17:33:29 +0530

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+ScaffoldApp::Application.config.secret_token = '73d0804622541f501bba55265d830e4c88577fa8ca2b62adac181e935a630df6f18c277b1dc9d3878d18f92dce8c43ee8c966e79cacb1eeea1ce5b4be7b21201'
